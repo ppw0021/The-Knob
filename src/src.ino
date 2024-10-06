76ac5d077767f0bd95898525699fa921f29b8b81
ppw0021 <ppw0021@autuni.ac.nz>
2024-10-07 00:31:37 +1300

    Code and Models Created

---
+++ src/src.ino
@@ -0,0 +1,59 @@
+//Arduino Leonardo
+
+//https://github.com/NicoHood/HID
+#include "HID-Project.h"
+
+//Pin definitions for rotary Encoder
+#define outputA 7
+#define outputB 6
+#define press 10
+
+//State variables
+int aState;
+int aLastState;
+bool pressed;
+
+//Run on startup
+void setup() {
+  //Start HID Library
+  Consumer.begin();
+
+  //Rotary PinModes
+  pinMode(outputA, INPUT);
+  pinMode(outputB, INPUT);
+  pinMode(press, INPUT);
+
+  //Read current state
+  aLastState = digitalRead(outputA);
+}
+
+//Loop
+void loop() {
+  //Check state
+  aState = digitalRead(outputA);
+  
+  //If state is different, check position 
+  if (aState != aLastState) {
+    if (digitalRead(outputB) != aState) {
+      //Twisted clockwise
+      Consumer.write(MEDIA_VOLUME_UP);
+    } else {
+      //Twisted counter-clockwise
+      Consumer.write(MEDIA_VOLUME_DOWN);
+    }
+  }
+
+  //Update last state
+  aLastState = aState;
+
+  //
+  if (digitalRead(press) && !pressed) {
+    pressed = true;
+    Consumer.write(MEDIA_VOLUME_MUTE);
+    delay(100);
+  }
+  if (!digitalRead(press) && pressed) {
+    pressed = false;
+    delay(100);
+  }
+}
